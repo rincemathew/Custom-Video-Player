ccfb68fa4bdc066db87cbf5c0bff116c5a1a47b2
Rince Mathew <rincemathewmanattu@gmail.com>
2022-12-21 18:46:06 +0530

    update progress and timestap

--- script.js
+++ script.js
@@ -22,6 +22,35 @@ function updatePlayIcon() {
   }
 }
 
+// Update progress & timestamp
+function updateProgress() {
+    progress.value = (video.currentTime / video.duration) * 100;
+  
+    // Get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if (mins < 10) {
+      mins = '0' + String(mins);
+    }
+  
+    // Get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if (secs < 10) {
+      secs = '0' + String(secs);
+    }
+  
+    timestamp.innerHTML = `${mins}:${secs}`;
+  }
+  
+  // Set video time to progress
+  function setVideoProgress() {
+    video.currentTime = (+progress.value * video.duration) / 100;
+  }
+  
+  // Stop video
+  function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+  }
 
 // Event listeners
 video.addEventListener('click', toggleVideoStatus);
